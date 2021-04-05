944954c34186ce49db307e807943a4fa0b91828a
tercoz <tercoz@mail.ru>
2021-04-05 10:49:26 +0300

    Spring session powered by JDBC (Postgres)

--- src/main/java/ignite/spring/session/config/WebSecurityConfig.java
+++ src/main/java/ignite/spring/session/config/WebSecurityConfig.java
@@ -1,10 +1,8 @@
 package ignite.spring.session.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -16,11 +14,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().requestMatchers(PathRequest.toH2Console());
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
